d4a25c5aeddc071b4c698d8f57cf027ac1dc5ce2
Phillip Wirth <phillip.wirth@dataport.de>
2025-01-15 11:19:33 +0100

    updated dependencies

--- cmd/kubectl-tap/mitmproxy.go
+++ cmd/kubectl-tap/mitmproxy.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -59,7 +59,7 @@ var MitmproxySidecarContainer = v1.Container{
 		},
 	},
 	ReadinessProbe: &v1.Probe{
-		Handler: v1.Handler{
+		ProbeHandler: v1.ProbeHandler{
 			HTTPGet: &v1.HTTPGetAction{
 				Path:   "/",
 				Port:   intstr.FromInt(kubetapProxyWebInterfacePort),
